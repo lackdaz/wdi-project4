d1407a3b262ffcdac4ea6fd189870062a0628c3c
seth loh <haloandwings@gmail.com>
2017-05-25 18:43:59 +0800

    fixed non live updates

--- app/controllers/chat_bot_controller.rb
+++ app/controllers/chat_bot_controller.rb
@@ -1,11 +1,15 @@
 require 'json'
 
 class ChatBotController < ApplicationController
-  @@file = File.read("#{Rails.root}/public/logic.json")
-  # @@file = File.read("./logic.json")
+  before_action :parse
 
   def index
     steps = JSON.parse(@@file)
     @chat_bot_props = { steps: steps }
+  end
+
+  private
+  def parse
+    @@file = File.read("#{Rails.root}/public/logic.json")
   end
 end
