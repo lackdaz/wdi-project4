1c5d3ad748815bd7359b1807fab0ae04502c40ab
seth loh <haloandwings@gmail.com>
2017-05-30 18:21:35 +0800

    what is wrong

--- client/app/bundles/Main/components/ReactSimpleChatBot/steps/custom/CustomStep.jsx
+++ client/app/bundles/Main/components/ReactSimpleChatBot/steps/custom/CustomStep.jsx
@@ -28,13 +28,14 @@ class CustomStep extends Component {
   }
 
   renderComponent() {
-    const { step, steps, previousStep, triggerNextStep } = this.props;
+    const { step, steps, previousStep, triggerNextStep, angerScore } = this.props;
     const { component } = step;
     return React.cloneElement(component, {
       step,
       steps,
       previousStep,
       triggerNextStep,
+      angerScore,
     });
   }
 
@@ -62,6 +63,7 @@ CustomStep.propTypes = {
   style: PropTypes.object.isRequired,
   previousStep: PropTypes.object.isRequired,
   triggerNextStep: PropTypes.func.isRequired,
+  angerScore: PropTypes.number,
 };
 
 export default CustomStep;
