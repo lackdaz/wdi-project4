42b6593f3a9745fdbaac5389469c13292bd17523
Raymond Aung <raymond.psaung@gmail.com>
2017-05-31 14:57:35 +0800

    heroku redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://postalcsbot.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://postalcsbot.herokuapp.com/', 'http://postalcsbot.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
